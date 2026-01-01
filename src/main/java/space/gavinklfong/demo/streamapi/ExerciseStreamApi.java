0499b16526086efb6dd4da0eb9db4a3e315676d2
minhngo248 <bbbbbb267@gmail.com>
2026-01-01 21:46:14 +0100

    :sparkles: Done all exercises

--- src/main/java/space/gavinklfong/demo/streamapi/ExerciseStreamApi.java
+++ src/main/java/space/gavinklfong/demo/streamapi/ExerciseStreamApi.java
@@ -2,18 +2,14 @@ package space.gavinklfong.demo.streamapi;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import space.gavinklfong.demo.streamapi.models.Customer;
 import space.gavinklfong.demo.streamapi.models.Order;
 import space.gavinklfong.demo.streamapi.models.Product;
 import space.gavinklfong.demo.streamapi.repos.CustomerRepo;
 import space.gavinklfong.demo.streamapi.repos.OrderRepo;
 import space.gavinklfong.demo.streamapi.repos.ProductRepo;
 
-import java.time.LocalDate;
 import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 /**
  * Learn how to use Java 8 Stream API
@@ -31,86 +27,107 @@ public class ExerciseStreamApi {
 
     public List<Product> exercise1() {
         // TODO: Obtain a list of product with category = "Books" and price > 100
+        return new ArrayList<>();
     }
 
     public List<Product> exercise1a() {
         // TODO: Obtain a list of product with category = "Books" and 
         //  price > 100 (using Predicate chaining for filter)
+        return new ArrayList<>();
     }
 
     public List<Product> exercise1b() {
         // TODO: Obtain a list of product with category = "Books" and price > 100 (using BiPredicate for filter)
+        return new ArrayList<>();
     }
 
     public List<Order> exercise2() {
         // TODO: Obtain a list of order with product category = "Baby"
+        return new ArrayList<>();
     }
 
     public List<Product> exercise3() {
         // TODO: Obtain a list of product with category = "Toys" and then apply 10% discount
+        return new ArrayList<>();
     }
 
     public List<Product> exercise4() {
         // TODO: Obtain a list of products ordered by customer of tier 2 between 01-Feb-2021 and 01-Apr-2021
+        return new ArrayList<>();
     }
 
     public List<Product> exercise5() {
         // TODO: Get the 3 cheapest products of "Books" category
+        return new ArrayList<>();
     }
 
     public List<Order> exercise6() {
         // TODO: Get the 3 most recent placed order
+        return new ArrayList<>();
     }
 
     public List<Product> exercise7() {
         // TODO: Get a list of products which was ordered on 15-Mar-2021
+        return new ArrayList<>();
     }
 
     public Double exercise8() {
         // TODO: Calculate the total lump of all orders placed in Feb 2021
+        return 0.0;
     }
 
     public Double exercise8a() {
         // TODO: Calculate the total lump of all orders placed in Feb 2021 (using reduce with BiFunction)
+        return 0.0;
     }
 
     public Double exercise9() {
         // TODO: Calculate the average price of all orders placed on 15-Mar-2021
+        return 0.0;
     }
 
     public DoubleSummaryStatistics exercise10() {
         // TODO: Obtain statistics summary of all products belong to "Books" category
+        return new DoubleSummaryStatistics();
     }
 
     public Map<Long, Integer> exercise11() {
         // TODO: Obtain a mapping of order id and the order's product count
+        return new HashMap<>();
     }
 
-    public void exercise12() {
+    public Map<Customer, List<Order>> exercise12() {
         // TODO: Obtain a data map of customer and list of orders
+        return new HashMap<>();
     }
 
-    public void exercise12a() {
+    public Map<Long, List<Long>> exercise12a() {
         // TODO: Obtain a data map of customer_id and list of order_id(s)
+        return new HashMap<>();
     }
 
-    public void exercise13() {
+    public Map<Order, Double> exercise13() {
         // TODO: Obtain a data map with order and its total price
+        return new HashMap<>();
     }
 
-    public void exercise13a() {
+    public Map<Order, Double> exercise13a() {
         // TODO: Obtain a data map with order and its total price (using reduce)
+        return new HashMap<>();
     }
 
-    public void exercise14() {
+    public Map<String, List<String>> exercise14() {
         // TODO: Obtain a data map of product name by category
+        return new HashMap<>();
     }
 
-    public void exercise15() {
+    public Map<String, Optional<Product>> exercise15() {
         // TODO: Get the most expensive product per category
+        return new HashMap<>();
     }
 
-    public void exercise15a() {
+    public Map<String, String> exercise15a() {
         // TODO: Get the most expensive product (by name) per category
+        return new HashMap<>();
     }
 }
--- src/main/java/space/gavinklfong/demo/streamapi/repos/OrderRepo.java
+++ src/main/java/space/gavinklfong/demo/streamapi/repos/OrderRepo.java
@@ -7,13 +7,33 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import space.gavinklfong.demo.streamapi.models.Order;
-import space.gavinklfong.demo.streamapi.models.Product;
 
 @Repository
 public interface OrderRepo extends CrudRepository<Order, Long> {
 
 	List<Order> findAll();
 
-	@Query("Select o FROM Order o JOIN FETCH o.products p WHERE p.category ='Baby'")
+	@Query("Select distinct o FROM Order o JOIN FETCH o.products p WHERE p.category ='Baby'")
 	List<Order> exercise2();
+
+	@Query("SELECT o FROM Order o ORDER BY o.orderDate DESC")
+	List<Order> exercise6();
+
+	@Query("select sum(p.price) FROM Order o JOIN o.products p WHERE o.orderDate >= '2021-02-01' AND o.orderDate < '2021-03-01'")
+	Double exercise8();
+
+	@Query("select avg(p.price) FROM Order o JOIN o.products p WHERE o.orderDate = '2021-03-15'")
+	Double exercise9();
+
+	@Query("SELECT o.id, COUNT(p) FROM Order o JOIN o.products p GROUP BY o.id")
+	List<Object[]> exercise11();
+
+	@Query("SELECT o.customer, o FROM Order o JOIN o.customer")
+	List<Object[]> exercise12();
+
+	@Query("SELECT o.customer.id, o.id FROM Order o")
+	List<Object[]> exercise12a();
+
+	@Query("SELECT o, SUM(p.price) FROM Order o JOIN o.products p GROUP BY o")
+	List<Object[]> exercise13();
 }
